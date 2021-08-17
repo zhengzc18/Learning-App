2b5fac9aee4839f725cf63254d9585a29ed6c338
Jaesoon <zhengzch18oon@gmail.com>
2021-08-17 17:02:05 +0900

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by JAESOON on 2021/08/17.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
